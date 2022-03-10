a2656a1aea217339dd7b040e6047e6e286cffa97
Fabi281 <Fabian.Meindl@freenet.de>
2022-03-10 14:34:12 +0100

    Added Interface

---
+++ src/de/dhbw/t2inf3001/pe/Formatter/AmericanFormatter.java
@@ -0,0 +1,27 @@
+package de.dhbw.t2inf3001.pe.Formatter;
+
+import de.dhbw.t2inf3001.pe.Address;
+import de.dhbw.t2inf3001.pe.PhoneNumber;
+import de.dhbw.t2inf3001.pe.Student;
+
+public class AmericanFormatter implements IFormatter{
+
+    @Override
+    public String formatPhoneNumber(PhoneNumber unformattedPhoneNumber) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String formatAddress(Address unformattedAddress) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String formatInfo(Student unformattedInfo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
