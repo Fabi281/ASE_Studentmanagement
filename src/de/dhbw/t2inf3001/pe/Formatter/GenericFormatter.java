a6137ef8440dafa6ab11ca4b65f48b16c22743f9
Finn Melzer <github@mfinn.de>
2022-03-21 23:04:52 +0100

    Shortened Generic Formatter and made it more extendable

--- src/de/dhbw/t2inf3001/pe/Formatter/GenericFormatter.java
+++ src/de/dhbw/t2inf3001/pe/Formatter/GenericFormatter.java
@@ -1,5 +1,8 @@
 package de.dhbw.t2inf3001.pe.Formatter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.dhbw.t2inf3001.pe.Address;
 import de.dhbw.t2inf3001.pe.PhoneNumber;
 import de.dhbw.t2inf3001.pe.Student;
@@ -12,30 +15,31 @@ public abstract class GenericFormatter {
 
     public abstract String formatInfo(Student unformattedInfo);
 
+    private static class CountryInfo {
+        public String countryCode;
+        public int cutoff;
+        public CountryInfo(String countryCode, int cutoff) {
+            this.countryCode = countryCode;
+            this.cutoff = cutoff;
+        }
+    }
+    private static final Map<String, CountryInfo> COUNTRY_INFO_MAP = new HashMap<String, CountryInfo>() {{
+        put("DE", new CountryInfo("49", 1));
+        put("GB", new CountryInfo("44", 1));
+        put("US", new CountryInfo("1", 0));
+        put("FR", new CountryInfo("33", 1));
+    }};
+
     public String formatPhoneNumberInternational(PhoneNumber unformattedPhoneNumber) {
         StringBuilder builder = new StringBuilder();
         builder.append("+");
-        switch (unformattedPhoneNumber.country) {
-            case "DE":
-                builder.append("49");
-                builder.append(unformattedPhoneNumber.areaCode.substring(1));
-                break;
-            case "GB":
-                builder.append("44");
-                builder.append(unformattedPhoneNumber.areaCode.substring(1));
-                break;
-            case "US":
-                builder.append("1");
-                builder.append(unformattedPhoneNumber.areaCode);
-                break;
-            case "FR":
-                builder.append("33");
-                builder.append(unformattedPhoneNumber.areaCode.substring(1));
-                break;
-            default:
-                builder.append(unformattedPhoneNumber.country);
-                builder.append(unformattedPhoneNumber.areaCode);
-                break;
+        CountryInfo countryInfo = COUNTRY_INFO_MAP.get(unformattedPhoneNumber.country);
+        if(countryInfo != null) {
+            builder.append(countryInfo.countryCode);
+            builder.append(unformattedPhoneNumber.areaCode.substring(countryInfo.cutoff));
+        } else {
+            builder.append(unformattedPhoneNumber.country);
+            builder.append(unformattedPhoneNumber.areaCode);
         }
         builder.append(unformattedPhoneNumber.subscriber);
         return builder.toString();
